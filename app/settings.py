0a82c0ce768d194e190fd6dbb8a7cd99943ca721
tavoxr <tavoxr23@gmail.com>
2021-09-03 15:14:48 -0600

    djdburl

--- app/settings.py
+++ app/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -104,7 +104,7 @@ DATABASES = {
     }
 }
 
-"""
+
 
 import dj_database_url
 
