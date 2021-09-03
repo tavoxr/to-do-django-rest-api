4dbc3c1f29b1039e7b74d0c51c23e6633832efb8
tavoxr <tavoxr23@gmail.com>
2021-09-02 19:01:04 -0600

    edit tasks

--- api/models.py
+++ api/models.py
@@ -5,6 +5,12 @@ from django.db import models
 class Task(models.Model):
     name = models.CharField(max_length=200)
     completed =  models.BooleanField(default=False, null=True, blank=True)
+    date_created =  models.DateTimeField(auto_now_add=True, null=True,blank=True)
+
+
+    class Meta:
+
+        ordering = ['-date_created',]
 
     def __str__(self):
         return f' {self.name} {self.completed}'
